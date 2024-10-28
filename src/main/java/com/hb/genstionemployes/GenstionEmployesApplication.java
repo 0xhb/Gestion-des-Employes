8b2cb246d2513199a96c994ce467e8afa7330fe0
El Housseine Boustta <boustta.elh@gmail.com>
2024-10-28 09:14:55 +0100

    Commit initiale

---
+++ src/main/java/com/hb/genstionemployes/GenstionEmployesApplication.java
@@ -0,0 +1,13 @@
+package com.hb.genstionemployes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenstionEmployesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenstionEmployesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hb/genstionemployes/GenstionEmployesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb.genstionemployes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenstionEmployesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
